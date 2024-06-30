0ea902cf5d37753ea98da76f7082edc0564014ba
JulioApprizaPay <julio.torres@apprizapay.com>
2024-06-29 19:18:02 -0600

    clone alviere-accounts-ios

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AccountsSDK",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "AccountsSDK",
+            type: .static,
+            targets: ["AccountsSDKDeps"]
+        )
+    ],
+    dependencies: [
+        .package(
+            name: "AlCore",
+            url: "https://github.com/Alviere/alviere-core-ios.git",
+            .exact("0.9.20")
+        ),
+        .package(
+            name: "AlCamera",
+            url: "https://github.com/Alviere/alviere-camera-ios.git",
+            .exact("1.0.6")
+        )
+    ],
+    targets: [
+        .target(
+             name: "AccountsSDKDeps",
+             dependencies: [
+                 "AccountsSDK",
+                 "AlCore",
+                 "AlCamera"
+             ],
+             path: "AccountsSDKDeps"
+         ),
+        .binaryTarget(
+            name: "AccountsSDK",
+            url: "https://github.com/Alviere/alviere-accounts-ios/releases/download/0.9.20/AccountsSDK.xcframework.zip",
+            checksum: "626aa57894def9cbff86d258a0248b4a1723cbe0bccdf94565646000ae97bf6a"
+        )
+    ]
+)
